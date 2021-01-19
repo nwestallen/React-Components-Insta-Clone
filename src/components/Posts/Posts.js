9197f686425dc2d5c0f0089b2e2531c69a8ff76d
Nick Allen <nwestallen@gmail.com>
2021-01-19 10:42:07 -0500

    got something on the page

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -8,6 +8,7 @@ const Posts = (props) => {
 
   return (
     <div className='posts-container-wrapper'>
+      {posts.map(itm => <Post post={itm} likePost={likePost} />)}
       {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
     </div>
